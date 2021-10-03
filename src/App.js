929bfeb6f4a32cc3f4b7cd92c151ba3e2799db4c
manish <manishpamnani169@gmail.com>
2021-10-03 13:31:35 +0530

    Footer Design.

--- src/App.js
+++ src/App.js
@@ -60,45 +60,44 @@ function App() {
 
           <div className="right-text">
 
-            {/* cards START*/}
-            <div class="container">
+            <div className="container">
               Sunday, October 3, 2021, week 39
               <br />
               <a href={url}>Day of German Unity</a>
               <p>Sun : Sunrise: 06:20 Sunset 18:11 <a href={url}>More Info</a></p>
-              <div class="row">
+              <div className="row">
 
-                <div class="col-sm">
+                <div className="col-sm">
                   One of three columns
                   One of three columns
                   One of three columns
                 </div>
 
-                <div class="col-sm">
+                <div className="col-sm">
                   One of three columns
                   One of three columns
                   One of three columns
                 </div>
 
-                <div class="col-sm">
+                <div className="col-sm">
                   One of three columns
                   One of three columns
                   One of three columns
                 </div>
 
-                <div class="col-sm">
+                <div className="col-sm">
                   One of three columns
                   One of three columns
                   One of three columns
                 </div>
 
-                <div class="col-sm">
+                <div className="col-sm">
                   One of three columns
                   One of three columns
                   One of three columns
                 </div>
 
-                <div class="col-sm">
+                <div className="col-sm">
                   One of three columns
                   One of three columns
                   One of three columns
@@ -106,14 +105,380 @@ function App() {
 
               </div>
             </div>
-            
-            {/* cards END */}
+            <br/>
 
 
         </div>
         </div>
       </div>
       {/* Content END */}
+
+      {/* Citites START */}
+      <div className="black-bg">
+              <div className="row">
+
+                <div className="col-sm-1">
+                  Jaipur
+                </div>
+
+                <div className="col-sm-1">
+                  Delhi
+                </div>
+
+                <div className="col-sm-1">
+                  Hyderabad
+                </div>
+
+                <div className="col-sm-1">
+                  Mumbai
+                </div>
+
+                <div className="col-sm-1">
+                  Pune
+                </div>
+
+                <div className="col-sm-1">
+                  Jaipur
+                </div>
+
+                <div className="col-sm-1">
+                  Delhi
+                </div>
+
+                <div className="col-sm-1">
+                  Hyderabad
+                </div>
+
+                <div className="col-sm-1">
+                  Mumbai
+                </div>
+
+                <div className="col-sm-1">
+                  Pune
+                </div>
+
+                <div className="col-sm-1">
+                  Jaipur
+                </div>
+
+                <div className="col-sm-1">
+                  Delhi
+                </div>
+
+                <div className="col-sm-1">
+                  Hyderabad
+                </div>
+
+                <div className="col-sm-1">
+                  Mumbai
+                </div>
+
+                <div className="col-sm-1">
+                  Pune
+                </div>
+
+                <div className="col-sm-1">
+                  Jaipur
+                </div>
+
+                <div className="col-sm-1">
+                  Delhi
+                </div>
+
+                <div className="col-sm-1">
+                  Hyderabad
+                </div>
+
+                <div className="col-sm-1">
+                  Mumbai
+                </div>
+
+                <div className="col-sm-1">
+                  Pune
+                </div>
+
+                <div className="col-sm-1">
+                  Jaipur
+                </div>
+
+                <div className="col-sm-1">
+                  Delhi
+                </div>
+
+                <div className="col-sm-1">
+                  Hyderabad
+                </div>
+
+                <div className="col-sm-1">
+                  Mumbai
+                </div>
+
+                <div className="col-sm-1">
+                  Pune
+                </div>
+
+                <div className="col-sm-1">
+                  Jaipur
+                </div>
+
+                <div className="col-sm-1">
+                  Delhi
+                </div>
+
+                <div className="col-sm-1">
+                  Hyderabad
+                </div>
+
+                <div className="col-sm-1">
+                  Mumbai
+                </div>
+
+                <div className="col-sm-1">
+                  Pune
+                </div>
+
+                <div className="col-sm-1">
+                  Jaipur
+                </div>
+
+                <div className="col-sm-1">
+                  Delhi
+                </div>
+
+                <div className="col-sm-1">
+                  Hyderabad
+                </div>
+
+                <div className="col-sm-1">
+                  Mumbai
+                </div>
+
+                <div className="col-sm-1">
+                  Pune
+                </div>
+
+                <div className="col-sm-1">
+                  Jaipur
+                </div>
+
+                <div className="col-sm-1">
+                  Delhi
+                </div>
+
+                <div className="col-sm-1">
+                  Hyderabad
+                </div>
+
+                <div className="col-sm-1">
+                  Mumbai
+                </div>
+
+                <div className="col-sm-1">
+                  Pune
+                </div>
+
+                <div className="col-sm-1">
+                  Jaipur
+                </div>
+
+                <div className="col-sm-1">
+                  Delhi
+                </div>
+
+                <div className="col-sm-1">
+                  Hyderabad
+                </div>
+
+                <div className="col-sm-1">
+                  Mumbai
+                </div>
+
+                <div className="col-sm-1">
+                  Pune
+                </div>
+
+                <div className="col-sm-1">
+                  Jaipur
+                </div>
+
+                <div className="col-sm-1">
+                  Delhi
+                </div>
+
+                <div className="col-sm-1">
+                  Hyderabad
+                </div>
+
+                <div className="col-sm-1">
+                  Mumbai
+                </div>
+
+                <div className="col-sm-1">
+                  Pune
+                </div>
+
+                <div className="col-sm-1">
+                  Jaipur
+                </div>
+
+                <div className="col-sm-1">
+                  Delhi
+                </div>
+
+                <div className="col-sm-1">
+                  Hyderabad
+                </div>
+
+                <div className="col-sm-1">
+                  Mumbai
+                </div>
+
+                <div className="col-sm-1">
+                  Pune
+                </div>
+
+                <div className="col-sm-1">
+                  Jaipur
+                </div>
+
+                <div className="col-sm-1">
+                  Delhi
+                </div>
+
+                <div className="col-sm-1">
+                  Hyderabad
+                </div>
+
+                <div className="col-sm-1">
+                  Mumbai
+                </div>
+
+                <div className="col-sm-1">
+                  Pune
+                </div>
+
+                <div className="col-sm-1">
+                  Jaipur
+                </div>
+
+                <div className="col-sm-1">
+                  Delhi
+                </div>
+
+                <div className="col-sm-1">
+                  Hyderabad
+                </div>
+
+                <div className="col-sm-1">
+                  Mumbai
+                </div>
+
+                <div className="col-sm-1">
+                  Pune
+                </div>
+
+                <div className="col-sm-1">
+                  Jaipur
+                </div>
+
+                <div className="col-sm-1">
+                  Delhi
+                </div>
+
+                <div className="col-sm-1">
+                  Hyderabad
+                </div>
+
+                <div className="col-sm-1">
+                  Mumbai
+                </div>
+
+                <div className="col-sm-1">
+                  Pune
+                </div>
+
+                <div className="col-sm-1">
+                  Jaipur
+                </div>
+
+                <div className="col-sm-1">
+                  Delhi
+                </div>
+
+                <div className="col-sm-1">
+                  Hyderabad
+                </div>
+
+                <div className="col-sm-1">
+                  Mumbai
+                </div>
+
+                <div className="col-sm-1">
+                  Pune
+                </div>
+
+              </div>
+            </div>
+      {/* Citites END */}
+
+      <br />
+
+      {/* Quote Section START */}
+      <div className="purple-bg">
+          <div className="row">
+            <div  className="container">
+              <p className="padding">
+                <blockquote>
+                  "Today is the only day. Yesterday is gone."
+                </blockquote>
+                - John Wooden
+              </p>
+            </div>
+          </div>
+          </div>
+      {/* Quote Section END */}
+
+      {/* Footer START */}
+      <footer className="w-100 py-4 flex-shrink-0">
+          <div className="container py-4">
+              <div className="row gy-4 gx-5">
+                  <div className="col-lg-4 col-md-6">
+                      <h5 className="h1 text-white">FB.</h5>
+                      <p className="small text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+                      <p className="small text-muted mb-0">&copy; Copyrights. All rights reserved. <a className="text-primary" href={url}>Bootstrapious.com</a></p>
+                  </div>
+                  <div className="col-lg-2 col-md-6">
+                      <h5 className="text-white mb-3">Quick links</h5>
+                      <ul className="list-unstyled text-muted">
+                          <li><a href={url}>Home</a></li>
+                          <li><a href={url}>About</a></li>
+                          <li><a href={url}>Get started</a></li>
+                          <li><a href={url}>FAQ</a></li>
+                      </ul>
+                  </div>
+                  <div className="col-lg-2 col-md-6">
+                      <h5 className="text-white mb-3">Quick links</h5>
+                      <ul className="list-unstyled text-muted">
+                          <li><a href={url}>Home</a></li>
+                          <li><a href={url}>About</a></li>
+                          <li><a href={url}>Get started</a></li>
+                          <li><a href={url}>FAQ</a></li>
+                      </ul>
+                  </div>
+                  <div className="col-lg-4 col-md-6">
+                      <h5 className="text-white mb-3">Newsletter</h5>
+                      <p className="small text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+                      <form action="#">
+                          <div className="input-group mb-3">
+                              <input className="form-control" type="text" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                              <button className="btn btn-primary" id="button-addon2" type="button"><i className="fas fa-paper-plane"></i></button>
+                          </div>
+                      </form>
+                  </div>
+              </div>
+          </div>
+      </footer>
+      {/* Footer END */}
     </div>
   );
 }
